283eee851328a9f3e839044e5f2800b8fdce40fe
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-07-09 14:21:45 +0100

    feature: addBook and getAllBooks

---
+++ src/books/routes.js
@@ -0,0 +1,15 @@
+const { Router } = require("express");
+const bookRouter = Router();
+
+const {addBook, getAllBooks} = require("./controllers")
+;
+// http://localhost:5001/books/test
+bookRouter.get("/test", async (req, res) => {
+    res.status(200).json({ message: "test a ok!"});
+});
+
+bookRouter.post("/addBook", addBook);
+
+bookRouter.get("/getAllBooks", getAllBooks);
+
+module.exports = bookRouter;
